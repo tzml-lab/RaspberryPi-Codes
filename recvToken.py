ea72164ebe47993d4ccf67ff330541a388b7f56f
Kai-Lun Huang <kailunhuang0904@gmail.com>
2019-09-30 16:48:38 +0800

    Update recvToken.py

--- recvToken.py
+++ recvToken.py
@@ -16,6 +16,14 @@ while True:
     print(addr)
     req = c.recv(1024)
     print("Recv:", req)
+    if int(req) == 1:
+       print("ON")
+       GPIO.output(7,GPIO.HIGH)
+       time.sleep(2)
+       print("OFF")
+       GPIO.output(7,GPIO.LOW)
+       c.send(b'success')	
+       continue
     with open("token", "w") as f:
         f.write(str(req, encoding="utf-8"))
 #    if int(req) == 1:
